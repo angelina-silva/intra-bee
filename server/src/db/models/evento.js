b28d032b7c7966b6af221e807edfee8e3ad803ee
ANGELINA INACIO DA SILVA <angelina.silva@alunos.sc.senac.br>
2021-09-21 11:36:12 -0300

    intra bee

---
+++ server/src/db/models/evento.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Evento extends Model {
+    
+    static associate(models) {
+      this.belongsToMany(models.Usuario, { through: "usuario_eventos" });
+    }
+  };
+  Evento.init({
+    nome: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    data: {
+      type: DataTypes.DATE,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Evento',
+  });
+  return Evento;
+};
